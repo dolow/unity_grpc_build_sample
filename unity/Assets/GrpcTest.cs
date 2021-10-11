057be5e493c17bb1e3f776fd66782ce202b85c4d
Smith <velvet.nanashi@gmail.com>
2021-10-11 13:45:48 +0900

    working project enum / oneof defs with iOS and Android build

--- unity/Assets/GrpcTest.cs
+++ unity/Assets/GrpcTest.cs
@@ -1,36 +1,27 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections;
+using System.IO;
 using System.Net.Http;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 using System.Text.RegularExpressions;
-using Grpc.Net.Client;
+//using Grpc.Net.Client;
 using Helloworld;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class GrpcTest : MonoBehaviour
 {
     [SerializeField]
     private UnityEngine.UI.Text uilog = null;
-    /*
-    private void Start()
-    {
-        string json = "{\\\"name\\\": \\\"セルリアンタワー東急ホテル\\\", \\\"category\\\": \\\"交通\\\", \\\"subcategory\\\": \\\"バス停\\\"}";
-        A c = JsonUtility.FromJson<A>(Regex.Unescape(json));
-        
-        Debug.Log(c.name);
-        Debug.Log(c.category);
-        Debug.Log(c.subcategory);
-    }
-    */
+
     public void RunCore()
     {
-        var reply = RunHelloWorld(false);
-        this.uilog.text = reply;
+        StartCoroutine(RunHelloWorld(false));
     }
     public void RunNet()
     {
-        var reply = RunHelloWorld(true);
-        this.uilog.text = reply;
+        StartCoroutine(RunHelloWorld(true));
     }
     class A
     {
@@ -38,39 +29,69 @@ public class GrpcTest : MonoBehaviour
         public string category;
         public string subcategory;
     }
+
+    private IEnumerator LoadAndroidAsset(string path)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(path))
+        {
+            yield return webRequest.SendWebRequest();
+            switch (webRequest.result)
+            {
+                case UnityWebRequest.Result.Success:
+                    yield return webRequest.downloadHandler.text; break;
+            }
+        }
+    }
+
+    private IEnumerator WaitLoadAssetContent(IEnumerator cursor)
+    {
+        object lastObject = null;
+        while (cursor.MoveNext())
+        {
+            lastObject = cursor.Current;
+            yield return (string)lastObject;
+        }
+    }
+
     // Can be run from commandline.
     // Example command:
     // "/Applications/Unity/Unity.app/Contents/MacOS/Unity -quit -batchmode -nographics -executeMethod HelloWorldTest.RunHelloWorld -logfile"
-    public string RunHelloWorld(bool useNet)
+    public IEnumerator RunHelloWorld(bool useNet)
     {
+        string path = Application.streamingAssetsPath;
 
-    string certPath = System.IO.Path.Combine(Application.streamingAssetsPath, "192.168.11.9.pem");
-        string keyPath = System.IO.Path.Combine(Application.streamingAssetsPath, "192.168.11.9-key.pem");
-        string caPath = System.IO.Path.Combine(Application.streamingAssetsPath, "rootCA.pem");
+        string certPath = System.IO.Path.Combine(path, "192.168.11.9.pem");
+        string keyPath = System.IO.Path.Combine(path, "192.168.11.9-key.pem");
+        string caPath = System.IO.Path.Combine(path, "rootCA.pem");
 
-        Grpc.Core.ChannelBase channel;
+        string caContent = "";
+        string certContent = "";
+        string keyContent = "";
 
-        if (useNet) {
-            HttpClientHandler handler = new HttpClientHandler();
-            handler.ServerCertificateCustomValidationCallback = (HttpRequestMessage request, X509Certificate2 certificate, X509Chain certificateChain, SslPolicyErrors policy) =>
-            {
-                return true;
-            };
-            handler.ClientCertificates.Add(new X509Certificate2(certPath));
-            HttpClient httpClient = new HttpClient(handler);
-            GrpcChannelOptions option = new GrpcChannelOptions();
-            channel = GrpcChannel.ForAddress("https://192.168.11.9:50051", new GrpcChannelOptions
-            {
-                HttpClient = httpClient
-                //HttpHandler = new Grpc.Net.Client.Web.GrpcWebHandler(handler)
-            });
-        } else {
-            Grpc.Core.SslCredentials credentials = new Grpc.Core.SslCredentials(
-                File.ReadAllText(caPath),
-                new Grpc.Core.KeyCertificatePair(File.ReadAllText(certPath), File.ReadAllText(keyPath))
-            );
-            channel = new Grpc.Core.Channel("192.168.11.9:50051", credentials);
-        }
+
+#if UNITY_ANDROID  && !UNITY_EDITOR
+        IEnumerator caEnum = LoadAndroidAsset(caPath);
+        IEnumerator keyEnum = LoadAndroidAsset(keyPath);
+        IEnumerator certEnum = LoadAndroidAsset(certPath);
+        yield return caEnum;
+        yield return caEnum;
+        caContent = (string)caEnum.Current;
+        yield return keyEnum;
+        yield return keyEnum;
+        keyContent = (string)keyEnum.Current;
+        yield return certEnum;
+        yield return certEnum;
+        certContent = (string)certEnum.Current;
+#else
+        caContent = File.ReadAllText(caPath);
+        certContent = File.ReadAllText(certPath);
+        keyContent = File.ReadAllText(keyPath);
+
+        yield return null;
+#endif
+
+        Grpc.Core.SslCredentials credentials = new Grpc.Core.SslCredentials(caContent, new Grpc.Core.KeyCertificatePair(certContent, keyContent));
+        Grpc.Core.ChannelBase channel = new Grpc.Core.Channel("192.168.11.9:50051", credentials);
 
         Greeter.GreeterClient client = new Greeter.GreeterClient(channel);
         
@@ -104,6 +125,6 @@ public class GrpcTest : MonoBehaviour
 
         channel.ShutdownAsync().Wait();
 
-        return log;
+        this.uilog.text = log;
     }
 }
